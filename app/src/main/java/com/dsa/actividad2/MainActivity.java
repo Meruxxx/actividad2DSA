3ea04da63dd36f94254e2ea32986b748af493106
juank <juank340@gmail.com>
2024-08-27 11:21:29 -0500

    cambios 3

--- app/src/main/java/com/dsa/actividad2/MainActivity.java
+++ app/src/main/java/com/dsa/actividad2/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             @Override
             public void onClick(View view) {
                 Toast.makeText(MainActivity.this, "Spizike", Toast.LENGTH_SHORT).show();
-                Intent intencion1 = new Intent(MainActivity.this, Craft.class);
+                Intent intencion1 = new Intent(MainActivity.this, spizike.class);
                 startActivity(intencion1);
             }
 
